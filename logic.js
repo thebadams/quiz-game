17e010b8c979edac81370de6031d321a21316284
thebadams <bpeteradams@gmail.com>
2021-02-27 13:10:37 -0500

    wrote countdownInterval functions

--- logic.js
+++ logic.js
@@ -1,15 +1,34 @@
 //countdown timer
-    //game over prompt
+var timer = document.querySelector(".timer-count"); //declare variable timer that is the countdown
+var countDownInterval 
+function interval() {
+    countDownInterval = setInterval(countDown, 1000)
+}
+function countDown() {
+    
+    var timeLeft = timer.textContent;
+    if(timeLeft > 0) {
+    timeLeft--
+    timer.textContent = timeLeft
+    } else {
+        clearInterval(countDownInterval);
+        alert("Game Over");
+        
+    }
+    
+}
 
-//array of words to choose from
+
 
 //keydown event listener
     //change the .word-blanks
+
+//
 
 //score keeper
     //reset score
 
 //start button
-
+    //array of words to choose from
 
 
