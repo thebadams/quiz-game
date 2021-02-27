082405523b6e526453995343f0085e2e02243bfe
mountaindew2141@outlook.com <mountaindew2141@outlook.com>
2021-02-27 13:19:55 -0500

    added comments to JS

--- logic.js
+++ logic.js
@@ -1,4 +1,5 @@
 //countdown timer
+
     //game over prompt
 
 //array of words to choose from
@@ -7,16 +8,16 @@
 
     //change the .word-blanks
     function renderBlanks() {
- 
+      // Randomly picks word from words array
       chosenWord = words[Math.floor(Math.random() * words.length)];
       lettersInChosenWord = chosenWord.split("");
       numBlanks = lettersInChosenWord.length;
       blanksLetters = []
-      
+      // Uses loop to push blanks to blankLetters array
       for (var i = 0; i < numBlanks; i++) {
         blanksLetters.push("_");
       }
-      
+      // Changes blankLetters array into a string and renders it on the screen
       wordBlank.textContent = blanksLetters.join(" ")
     }
 //score keeper
