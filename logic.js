4fb8ad353d2e2bd839dd5bf00b67dfc06de46d31
mountaindew2141@outlook.com <mountaindew2141@outlook.com>
2021-02-27 13:12:20 -0500

    created blanks on screen, then function chooses words from the array, then loops to push blanks to blankLetters array

--- logic.js
+++ logic.js
@@ -4,12 +4,32 @@
 //array of words to choose from
 
 //keydown event listener
-    //change the .word-blanks
 
+    //change the .word-blanks
+    function renderBlanks() {
+ 
+      chosenWord = words[Math.floor(Math.random() * words.length)];
+      lettersInChosenWord = chosenWord.split("");
+      numBlanks = lettersInChosenWord.length;
+      blanksLetters = []
+      
+      for (var i = 0; i < numBlanks; i++) {
+        blanksLetters.push("_");
+      }
+      
+      wordBlank.textContent = blanksLetters.join(" ")
+    }
 //score keeper
     //reset score
 
 //start button
+
+
+
+
+
+
+
 
 
 
