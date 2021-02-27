c7a0f74a448f9d2f6d93c55bd88811867676baa7
C Blackwell <bmathematics@gmail.com>
2021-02-27 13:15:28 -0500

    changes to word choice

--- logic.js
+++ logic.js
@@ -1,15 +1,26 @@
+var start = document.querySelector(".start-button");
+var blanks = document.querySelector(".word-blanks");
+var words = ["J _ v _ S c r _ _t", "Q _ _ r _", "_ t t r _ b _ t _", "_ r g _ m _ n _", "W _ _ d _ w", "_ o c _ m _ _ t" ]
 //countdown timer
     //game over prompt
 
-//array of words to choose from
+
 
 //keydown event listener
     //change the .word-blanks
 
+
+
 //score keeper
     //reset score
 
+
+
 //start button
-
-
-
+//array of words to choose from
+// start.addEventListener("click", wordChoice(){
+// })
+function wordChoice(){
+    var item = words[Math.floor(Math.random() * words.length)];
+        blanks.textContent = item;
+};
